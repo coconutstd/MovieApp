af57141ff806e583c2088c3ecc5f4e3704c142ce
Luke <luke@tessa.art>
2022-06-19 08:44:15 +0900

    Theme 적용

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import React, {useState} from 'react';
-import * as Font from 'expo-font'
+import React from 'react';
 import AppLoading from "expo-app-loading";
-import {Text, Image} from "react-native";
 import {Ionicons} from "@expo/vector-icons"
-import { Asset, useAssets } from "expo-asset"
+import {useAssets} from "expo-asset"
 import {useFonts} from "expo-font";
 import {NavigationContainer} from "@react-navigation/native";
 import Root from "./navigation/Root";
+import { Image, useColorScheme } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.jpeg")])
   const [loaded] = useFonts(Ionicons.font)
+  const isDark = useColorScheme() === "dark";
+
   if (!assets || !loaded) {
     return <AppLoading />
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
-import {TouchableOpacity} from "react-native";
 import styled from 'styled-components/native'
 
 const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor}
 `;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor}
+`
 
 
 const Movies = ({ navigation: {navigate}}) => (
   <Btn onPress={() => navigate("Stack", { screen: "Three"})}>
-    <Text>Movies</Text>
+    <Title>Movies</Title>
   </Btn>
 );
 export default Movies;
