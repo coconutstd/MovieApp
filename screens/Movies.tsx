7ee2a2974604b0e349db6899299cc6d081423051
Luke <luke@tessa.art>
2022-06-20 16:33:01 +0900

    movie API fetch 확인

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styled from 'styled-components/native'
+import Swiper from "react-native-web-swiper";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {ActivityIndicator, Dimensions} from "react-native";
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor}
-`;
+const API_KEY = "2f7e1fdd60dee73dda1c0d7981150b8e"
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor}
+const Container = styled.ScrollView`
+  background-color: ${props => props.theme.mainBgColor};
 `
 
+const View = styled.View`
+  flex: 1;
+`
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({ navigation}) => (
-  <Btn onPress={() => navigation.navigate("Stack", { screen: "Three"})}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+`
+
+const {height: SCREEN_HEIGHT} = Dimensions.get("window")
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+    const [loading, setLoading] = useState(true)
+    const getNowPlaying = async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`)
+        const json = await response.json()
+        console.log(json)
+    }
+    useEffect(() => {
+        getNowPlaying()
+    }, [])
+    return loading ? (
+        <Loader>
+            <ActivityIndicator/>
+         </Loader>
+    ) : (
+        <Container>
+        <Swiper loop timeout={3.5} controlsEnabled={false} containerStyle={{width: "100%", height: SCREEN_HEIGHT / 4}}>
+            <View style={{backgroundColor: "red"}}></View>
+            <View style={{backgroundColor: "blue"}}></View>
+            <View style={{backgroundColor: "red"}}></View>
+            <View style={{backgroundColor: "blue"}}></View>
+        </Swiper>
+    </Container>
+    )
+}
+
 export default Movies;
